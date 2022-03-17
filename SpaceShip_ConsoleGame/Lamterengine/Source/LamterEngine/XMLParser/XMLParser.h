8fd92f0f31092b54d2c7cf5071e98aab32fd4833
JoanOrtiga <joanorba@gmail.com>
2022-03-17 15:06:36 +0100

    XML parser

---
+++ SpaceShip_ConsoleGame/Lamterengine/Source/LamterEngine/XMLParser/XMLParser.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+
+#include "../../ThirdParty/tinyxml2.h"
+
+class XMLParser
+{
+public:
+	template <class T>
+	static void SaveObject(T t, std::string fileLocation)
+	{
+		int x = 5;
+		tinyxml2::XMLDocument doc;
+		doc.LoadFile(fileLocation.c_str());
+		doc.Parse()
+	}
+};
+
+class data
+{
+	int x;
+	int y;
+};
+
+void x()
+{
+	data t;
+	XMLParser::SaveObject(t, "");
+}
